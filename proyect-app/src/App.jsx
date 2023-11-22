c2f398e3dd25458129329ea9b83e63a6bfd0cc2c
ManuPerezMagro <manuperezmagro@gmail.com>
2023-11-22 10:05:17 -0300

    segunda pre-entrega

--- proyect-app/src/App.jsx
+++ proyect-app/src/App.jsx
@@ -1,18 +1,16 @@
-import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
-import { NavBar } from "./components/NavBar/NavBar"
+import { Cart, GifLoading, ItemDetailContainer, ItemListContainer, NavBar } from "./components"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-
-
-function App() {
-
+export const App = () => {
   return (
-    <>
-    <NavBar />
-    <div className="text-center p-5 fs-2">
-    <ItemListContainer greeting={"Bienvenido a Tech-House. Casa de Electronica"}/>
-    </div>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:category" element={<ItemListContainer />} />
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
--- proyect-app/src/components/NavBar/NavBar.jsx
+++ proyect-app/src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { CartWidget } from "./CartWidget/CartWidget"
 import style from "./NavBar.module.css"
 
@@ -7,37 +8,63 @@ export const NavBar = () => {
         <div>
             <nav className={`navbar navbar-expand-lg navBarColor ${style.navBarColor}`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand fs-2"  href="#">Tech-House</a>
+                    <Link to="/">
+                    <a className="navbar-brand fs-2" >Tech-House</a>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
                             <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link to="/">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" href="#">Placas</a>
+                                <Link to="/category/grafica">
+                                <a className="nav-link" href="#">Graficas</a>
+                                </Link>
                             </li>
                             <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Perifericos
-                            </a>
+                                </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Mouse</a></li>
-                                <li><a className="dropdown-item" href="#">Teclado</a></li>
-                                <li><a className="dropdown-item" href="#">Auriculares</a></li>
-                                <li><a className="dropdown-item" href="#">Microfono</a></li>
+                                <li>
+                                    <Link to={"/category/mouse"}>
+                                    <a className="dropdown-item" href="#">Mouse</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/category/teclado">
+                                    <a className="dropdown-item" href="#">Teclado</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/category/auriculares">
+                                    <a className="dropdown-item" href="#">Auriculares</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/category/microfonos">
+                                    <a className="dropdown-item" href="#">Microfono</a>
+                                    </Link>
+                                </li>
                             </ul>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" aria-disabled="true">Monitores</a>
+                                <Link to="/category/monitores">
+                                <a className="nav-link" aria-disabled="true">Monitores</a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
                 </div>
                 <div>
-                    <CartWidget/>
+                    <Link to="/Cart">
+                        <CartWidget/>
+                    </Link>
                 </div>
             </nav>
         </div>
